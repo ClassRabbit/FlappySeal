ebe5d98f6c0b52e1d3da34c0a5e7ba63f161ddaa
KMC <kmc@KMCui-MacBook-Pro.local>
2017-01-14 17:30:39 +0900

    캐릭터 중력효과 적용

---
+++ Assets/Scripts/SealController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SealController : MonoBehaviour {
+
+	Rigidbody2D rb2d;
+	Animator animator;
+	float angle;
+
+	public float maxHeight;
+	public float flapVelocity;
+	public float relativeVelocityX;
+	public GameObject sprite;
+
+	void Awake()
+	{
+		rb2d = GetComponent<Rigidbody2D>();
+		animator = sprite.GetComponent<Animator>();
+	}
+
+	void Update()
+	{
+		if (Input.GetButtonDown("Fire1") && transform.position.y < maxHeight)
+		{
+			Flap();
+		}
+
+		ApplyAngle();
+
+		animator.SetBool("flap", angle >= 0.0f);
+	}
+
+	public void Flap()
+	{
+		rb2d.velocity = new Vector2(0.0f, flapVelocity);
+	}
+
+	void ApplyAngle()
+	{
+		float targetAngle = Mathf.Atan2(rb2d.velocity.y, relativeVelocityX) * Mathf.Rad2Deg;
+
+		angle = Mathf.Lerp(angle, targetAngle, Time.deltaTime * 10.0f);
+
+		sprite.transform.localRotation = Quaternion.Euler(0.0f, 0.0f, angle);
+	}
+
+}
